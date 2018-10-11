8f4060cf8bc76b11e48e06b684838f7e4f6e250f
yangyi <yhxyangyi@163.com>
2018-10-11 11:42:08 +0800

    终端命令

--- src/render/pages/project/detail.js
+++ src/render/pages/project/detail.js
@@ -1,8 +1,10 @@
 import React, { Component}from 'react';
-import { NavLink } from "react-router-dom";
+import electron from 'electron';
 import "./style.scss";
 import {Icon}from 'antd'
 import DetailModel from './components/detailModel'
+const { ipcRenderer, shell } = electron;
+const { app, dialog } = electron.remote;
 export default class Detail extends Component {
     constructor (props){
         super(props);
@@ -19,11 +21,27 @@ export default class Detail extends Component {
             des:'test页面'
            }
          ],
-         isShow:true
+         isShow:false
      }
     }
   setPage=()=>{
    this.setState({isShow:!this.state.isShow});
+  }
+  openFloder=()=>{
+    dialog.showOpenDialog({
+        title: 'Open Project',
+        properties:['openFile'],
+        // filters: [
+        //   { name: 'test', extensions: extensions},
+        // ],
+      }, (file) => {
+        if (file) {
+          //this._readData(file[0], callBack);
+        }
+      });
+  }
+  openUrl=()=>{
+    shell.openExternal('https://electronjs.org');
   }
   render() {
       const{isShow,pageData}=this.state;
@@ -39,9 +57,9 @@ export default class Detail extends Component {
             <Icon type="cloud-download" theme="outlined" />
           </div>
           <div className="head-right fr">
-            <i className="iconfont icon-iconset0196"> 文件夹</i>
-            <i className="iconfont icon-daima"> 源码</i>
-            <i className="iconfont icon-plus-preview"> 预览</i>
+            <i className="iconfont icon-iconset0196" onClick={this.openFloder}> 文件夹</i>
+            <i className="iconfont icon-daima" onClick={this.openUrl} > 源码</i>
+            <i className="iconfont icon-plus-preview" onClick={this.openUrl}> 预览</i>
           </div>
         </div>
         <div className="detail-content">
